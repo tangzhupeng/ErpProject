cdb66154141edfc8784af9fb9601ec0e3a772bf7
Lanyno <81664032+Lanyno@users.noreply.github.com>
2021-04-04 19:51:00 +0800

    1

--- src/main/java/cn/gson/jindie/model/pojos/capitalpojos/ErpReceiptRecord.java
+++ src/main/java/cn/gson/jindie/model/pojos/capitalpojos/ErpReceiptRecord.java
@@ -51,16 +51,16 @@ public class ErpReceiptRecord {
         this.accountId = accountId;
     }
 
-
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    public ErpOrder getOrder() {
-        return order;
-    }
-
-    public void setOrder(ErpOrder order) {
-        this.order = order;
-    }
+//
+//    @ManyToOne
+//    @JoinColumn(name = "order_id")
+//    public ErpOrder getOrder() {
+//        return order;
+//    }
+//
+//    public void setOrder(ErpOrder order) {
+//        this.order = order;
+//    }
 
     @Basic
     @Column(name = "re_way")
