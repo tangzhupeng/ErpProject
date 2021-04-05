7912771ade691a8e3a6c71c63510d5604a6e7354
tangzhupeng <2316415646@qq.com>
2021-04-05 14:24:47 +0800

    fukuan
    付款页面

---
+++ src/main/java/cn/gson/jindie/model/service/capitalservice/ReceiptService.java
@@ -0,0 +1,9 @@
+package cn.gson.jindie.model.service.capitalservice;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ReceiptService {
+}
