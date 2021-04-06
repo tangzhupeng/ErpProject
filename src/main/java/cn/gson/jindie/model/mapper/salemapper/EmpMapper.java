9ea7d5b35d0cd62105021113cf38c0e73fd84395
Lanyno <81664032+Lanyno@users.noreply.github.com>
2021-04-06 22:23:44 +0800

    Update EmpMapper.java

--- src/main/java/cn/gson/jindie/model/mapper/salemapper/EmpMapper.java
+++ src/main/java/cn/gson/jindie/model/mapper/salemapper/EmpMapper.java
@@ -1,13 +1,13 @@
-package cn.gson.jindie.model.mapper.salemapper;
-
-import cn.gson.jindie.model.pojos.PerPojos.ErpEmp;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
-@Repository
-@Mapper
-public interface EmpMapper {
-    List<ErpEmp> personnelEmpClientList();
-}
+//package cn.gson.jindie.model.mapper.salemapper;
+//
+//import cn.gson.jindie.model.pojos.perpojos.ErpEmp;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//@Mapper
+//public interface EmpMapper {
+////    List<ErpEmp> personnelEmpClientList();
+//}
